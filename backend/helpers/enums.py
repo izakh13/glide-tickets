323b7410e2abd1c00fc980f84ecdba46a7e94472
izakh13 <izakh13g@users.noreply.github.com>
2025-03-13 10:19:27 +0000

    add helpers package

---
+++ backend/helpers/enums.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+class TicketTypes(Enum):
+    CHOOSE_VALUE = ('choose_value', '--Choose a value--')
+    ADULT = ('adult', 'Adult')
+    CHILDREN = ('children', 'Children')
+
+
+    def __new__(cls, value, description):
+        obj = object.__new__(cls)
+        obj._value_ = value
+        obj.description = description
+        return obj
+
+    def __str__(self):
+        return f'("{self.value}", "{self.description}")'
+    
+
+    @staticmethod
+    def get_all():
+        res = []
+        for value in TicketTypes.__members__.values():
+            res.append(eval(str(value)))
+        return tuple(res)
