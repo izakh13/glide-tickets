4665a5a8f95f8eb0adf1ae668c80ef0ea9cc432c
izakh13 <izakh13g@users.noreply.github.com>
2025-03-14 09:18:47 +0000

    add TupleEnum class

--- backend/helpers/enums.py
+++ backend/helpers/enums.py
@@ -1,46 +1,30 @@
 from enum import Enum
 
-class TicketTypes(Enum):
+class TupleEnum(Enum):
+    def __new__(cls, value, description):
+        obj = object.__new__(cls)
+        obj._value_ = value
+        obj.description = description
+        return obj
+
+    def __str__(self):
+        return f'("{self.value}", "{self.description}")'
+    
+    @classmethod
+    def get_all(cls):
+        res = []
+        for value in cls.__members__.values():
+            res.append(eval(str(value)))
+        return tuple(res)
+
+class TicketTypes(TupleEnum):
     CHOOSE_VALUE = ('choose_value', '--Choose a value--')
     ADULT = ('adult', 'Adult')
     CHILDREN = ('children', 'Children')
 
 
-    def __new__(cls, value, description):
-        obj = object.__new__(cls)
-        obj._value_ = value
-        obj.description = description
-        return obj
-
-    def __str__(self):
-        return f'("{self.value}", "{self.description}")'
-    
-
-    @staticmethod
-    def get_all():
-        res = []
-        for value in TicketTypes.__members__.values():
-            res.append(eval(str(value)))
-        return tuple(res)
-
-class MeansOfTransport(Enum):
+class MeansOfTransport(TupleEnum):
     CHOOSE_VALUE = ('choose_value', '--Choose a value--')
     AIRPLANE = ('airplane', 'Airplane')
     COACH = ('coach', 'Coach')
     TRAIN = ('train', 'Train')
-
-    def __new__(cls, value, description):
-        obj = object.__new__(cls)
-        obj._value_ = value
-        obj.description = description
-        return obj
-
-    def __str__(self):
-        return f'("{self.value}", "{self.description}")'
-    
-    @staticmethod
-    def get_all():
-        res = []
-        for value in MeansOfTransport.__members__.values():
-            res.append(eval(str(value)))
-        return tuple(res)
