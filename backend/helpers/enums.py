30a94da31a350c8531518a9d39dda605f3d81103
izakh13 <izakh13g@users.noreply.github.com>
2025-03-14 08:55:49 +0000

    add enum for means of trans

--- backend/helpers/enums.py
+++ backend/helpers/enums.py
@@ -22,3 +22,25 @@ class TicketTypes(Enum):
         for value in TicketTypes.__members__.values():
             res.append(eval(str(value)))
         return tuple(res)
+
+class MeansOfTransport(Enum):
+    CHOOSE_VALUE = ('choose_value', '--Choose a value--')
+    AIRPLANE = ('airplane', 'Airplane')
+    COACH = ('coach', 'Coach')
+    TRAIN = ('train', 'Train')
+
+    def __new__(cls, value, description):
+        obj = object.__new__(cls)
+        obj._value_ = value
+        obj.description = description
+        return obj
+
+    def __str__(self):
+        return f'("{self.value}", "{self.description}")'
+    
+    @staticmethod
+    def get_all():
+        res = []
+        for value in MeansOfTransport.__members__.values():
+            res.append(eval(str(value)))
+        return tuple(res)
