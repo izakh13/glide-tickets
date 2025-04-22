a3e29ea0183e251acc189940822378f7c4ed15ff
izakh13 <izakh13g@users.noreply.github.com>
2025-04-22 08:01:54 +0000

    add accounts

---
+++ accounts/froms.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AdminPasswordChangeForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email')
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = ('username')
+
+
+class CustomUserPasswordChangeForm(AdminPasswordChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = ('email')
